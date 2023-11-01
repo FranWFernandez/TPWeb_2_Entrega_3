fd7eae0f9363be5b800c3023ceca752554bc458f
FranWFernandez <145360524+FranWFernandez@users.noreply.github.com>
2023-11-01 17:03:21 -0300

    modifico los models

--- aplicacion/models/marcas.model.php
+++ aplicacion/models/marcas.model.php
@@ -3,7 +3,6 @@
 require_once './aplicacion/models/model.php';
 
 class MarcasModel extends DB {
-
     function getMarcasNames() {
         $query = $this->connect()->prepare('SELECT * FROM marcas');
         $query->execute();
@@ -12,18 +11,23 @@ class MarcasModel extends DB {
 
         return $marcas;
     }
-
-
-    // function insertMarca($marca) {
-    //     $query = $this->connect()->prepare('INSERT INTO marcas (marca) VALUES(?)');
-    //     $query->execute([$marca]);
-
-    //     return $this->connect()->lastInsertId();
-    // }
-
-    
-    // function deleteMarca($id) {
-    //     $query = $this->connect()->prepare('DELETE FROM marcas WHERE id_marca = ?');
-    //     $query->execute([$id]);
-    // }
+    public function getMarcabyID ($id){
+        $query= $this->connect()->prepare('SELECT * FROM marcas WHERE id_marca = ?');
+        $query->execute([$id]);
+        $Item = $query->fetch(PDO::FETCH_OBJ);
+        return $Item;
+    }
+    function insertMarca($marca) {
+        $query = $this->connect()->prepare('INSERT INTO marcas (marca) VALUES(?)');
+        $query->execute([$marca]);
+        return $this->connect()->lastInsertId();
+    }
+    function deleteMarca($id) {
+        $query = $this->connect()->prepare('DELETE FROM marcas WHERE id_marca = ?');
+        $query->execute([$id]);
+    }
+    function updateMarca($id, $marca) {
+        $query = $this->connect()->prepare('UPDATE marcas SET marca=? WHERE id_marca =?');
+        $query->execute([$marca, $id]);
+    } 
 }
